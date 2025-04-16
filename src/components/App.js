b7ccc9f48dc5c4ee1a80959399202a7f96019b27
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-04-16 05:56:41 +0300

    adding home and about component

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+// ✅ Write an <About> component here
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {/* ✅ Add the <Home> component here */}
+      <Home />
+      {/* ✅ Add your <About> component here */}
+      <About />
     </div>
   );
 }
